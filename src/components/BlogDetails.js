7a79ddd95a97b495a9b207129792cb9c66d90475
Alexandre <alexandre@weel.com>
2022-01-23 15:49:17 +0200

    adding option to post

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,48 +1,32 @@
-import { useParams} from "react-router";
+import { useParams } from "react-router";
 import { useState, useEffect } from "react";
+
 const BlogDetails = () => {
-    
-    //get id from url
-    const {id} = useParams()
-    console.log(id)
-    
-    const [blog,setBlog] = useState([])
-    const loadData = () => {
+    console.log(useParams())
+    //get id from url using distructure in "useParams"
+    const { id } = useParams()
+
+    const [blog, setBlog] = useState([])
+    const loadDataById = () => {
         fetch("http://localhost:8000/blogs/" + id)
-        .then(res => res.json())
-        .then(data => {
-            setBlog(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                setBlog(data)
+            })
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log("useEffect -> blogdetails")
-        loadData()
-}, [])
+        loadDataById()
+    }, [])
 
-
-//http request from server
-
-
-
-
-
-// mock data ----
-// let id = 1
-let b = {
-    title: "Title",
-    body: "body",
-    author: "author"
-}
-// ---------------
-
-    return (  
-            <div>
-                <h1>{blog.title}</h1>
-                <h3>Author: {blog.author}</h3>
-                <p>{blog.body}</p>
-            </div>
-        )
-    };
+    return (
+        <div>
+            <h1>{blog.title}</h1>
+            <h3>Author: { blog.author}</h3>
+            <p>{blog.body}</p>
+        </div>
+    )
+};
 
 export default BlogDetails;
--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+
+  // const [input, setInput] = useState('')
+  // const [title, setTitle] = useState('')
+  // const [author, setAuthor] = useState('')
+
+
+  // const addBlog = (blogObject) =>
+  //   fetch("http://localhost:8000/blogs/", {
+  //       method: 'POST',
+  //       headers: {
+  //           'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(blogObject)
+  //   }).then(res => res.json());
+  
+
+
     return (
       <div className="CreateBlog-root">
           <div className='Create-blog-main-container'>
@@ -7,25 +26,25 @@ const CreateBlog = () => {
           <form className="CreateBlog-form">
             <label for="blog-title">Blog Title</label>
             <br/>
-            <input id="blog-title" type="text" required/>
+            <input id="blog-title" type="text" required  />
             <br/>
             <label for="blog-author">Author</label>
             <br/>
-            <input type="text" id="blog-author" required/>
+            <input type="text" id="blog-author" required />
             <br/>
             <label for="blog-subject">Subject</label>
             <br/>
-            <input id= "blog-subject" type="text" required/>
+            <input id= "blog-subject" type="text" required />
             <br/>
             <label for="blog-content">Blog Content</label>
             <br/>
-            <textarea required id="blog-content" rows="4" cols="50"/>
+            <textarea required id="blog-content" rows="4" cols="50" />
             <br/>
             <button>Submit</button>
           </form>
           </div>
         </div>
     );
-  }
+    }
 
 export default CreateBlog
